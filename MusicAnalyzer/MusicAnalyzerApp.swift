c539caed86c7a453fba1471b53059c08e60db9cb
Matkat Music LLC <matkatmusic@gmail.com>
2024-04-29 18:24:01 -0700

    Initial Commit

---
+++ MusicAnalyzer/MusicAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAnalyzerApp.swift
+//  MusicAnalyzer
+//
+//  Created by Matkat Music LLC on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
